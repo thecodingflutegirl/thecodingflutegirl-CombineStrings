1920d90ebdbe2f226ce255ad392f64a561cfb9b7
Julia Evers <thatflutegirl12@gmail.com>
2023-03-01 19:16:01 +0000

    completed combine strings lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -9,6 +9,7 @@ public class Lab {
      * @return the combination of strings a and b.
      */
     public String combine(String a, String b){
-        return "";
+        String combinedStrings = a + b;
+        return combinedStrings;
     }
 }
